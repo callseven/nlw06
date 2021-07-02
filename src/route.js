3fc043fd1a04e357bc316e335a9f3cdf8eff97b0
callseven <callseven@me.com>
2021-07-01 23:15:14 -0300

    update

--- src/route.js
+++ src/route.js
@@ -6,4 +6,7 @@ route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req, res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("create-pass"))
 
+// formato que o formulario de dentro da modal tem que passar a informacao
+// route.post('/romm/:romm/:question/:action', (req, res) => res.render("exemplo", {req}))
+
 module.exports = route
