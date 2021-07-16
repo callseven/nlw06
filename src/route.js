2c622e7f5bd50153bc5c8a535f14e293cce2bbfb
callseven <callseven@me.com>
2021-07-15 21:11:27 -0300

    update

--- src/route.js
+++ src/route.js
@@ -1,13 +1,16 @@
 const express  = require("express")
 const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"))
-route.get('/room', (req, res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
+
+route.get('/room/:room', (req, res) => res.render("room"))
 
 // formato que o formulario de dentro da modal tem que passar a informacao
 route.post('/romm/:romm/:question/:action', QuestionController.index)
+route.post('/romm/create-romm/', RoomController.create)
 
 module.exports = route
